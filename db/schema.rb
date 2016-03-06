2408f0ab75143e845e2f4a697aa04e7d0d27e8b5
luizromariofilho <luizromariofilho@gmail.com>
2016-03-06 18:37:17 -0300

    Add users functions.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221012037) do
+ActiveRecord::Schema.define(version: 20160305162919) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20160221012037) do
     t.string   "password"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
