39f92f81c957662590d3ed1079714dc050679b91
luizromariofilho <luizromariofilho@gmail.com>
2016-03-06 23:40:03 -0300

    Add criptografia no modelo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-	validates_presence_of :email, :full_name, :location, :password
-	validates_confirmation_of :password
+	validates_presence_of :email, :full_name, :location
 	validates_length_of :bio, minimum:30, allow_blank: false
 	validates_uniqueness_of :email
+
+	has_secure_password
 end
